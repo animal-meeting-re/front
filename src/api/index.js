acca0da13c3e5c0514773a97332824de0bd81c36
DaeWon9 <qkreodnjs97@naver.com>
2024-05-26 01:27:31 +0900

    Fix : sendResultImage api

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 const BACK_BASE_URL = process.env.REACT_APP_BACK_URL;
 
-export const sendResultImage = async (formData) => {
+export const sendResultImage = async (gender, formData) => {
+    const url =
+        gender === "MALE" ? `${BACK_BASE_URL}/measurements/upload/male` : `${BACK_BASE_URL}/measurements/upload/female`;
+
     const response = await axios
-        .post(`${BACK_BASE_URL}/measurements/upload/male`, formData, {
+        .post(url, formData, {
             headers: {
                 "Content-Type": "multipart/form-data",
             },
         })
         .then((res) => {
-            console.log(res);
+            console.log("TEST" + res);
             return response;
         });
 };
