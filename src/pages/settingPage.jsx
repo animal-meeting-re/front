45a76262b7aaf12531bcf3b8d9cef7848b8ca1fc
sehxxnee <shyan5253@naver.com>
2024-05-25 23:12:29 +0900

    Feat: participant-wrapper, noticeboard-wrapper 반응형으로 나타 내도록 수정

--- src/pages/settingPage.jsx
+++ src/pages/settingPage.jsx
@@ -8,18 +8,18 @@ import Statistic from "../components/Statistic";
 import "./settingPage.css";
 
 export const Row = ({children}) => {
-  const isRow = useMediaQuery({
-    query:"(max-width:2000px)"
-  });
-  return <>{isRow && children}</>
-}
-
-export const Column = ({children}) => {
-  const isColumn = useMediaQuery({
-    query:"(max-width:1000px)"
-  });
-  return <>{isColumn && children}</>
-}
+    const isRow = useMediaQuery({
+      query:"(min-width:501px)"
+    });
+    return <>{isRow && children}</>
+  }
+  
+  export const Column = ({children}) => {
+    const isColumn = useMediaQuery({
+      query:"(min-width:500px)"
+    });
+    return <>{isColumn && children}</>
+  }
 /*
 function App () {
   return (
@@ -51,102 +51,111 @@ const TypingText = ({ text, onFinish }) => {
 };
 
 const SettingPage = () => {
-  const navigate = useNavigate();
-  const [selectedGender, setSelectedGender] = useRecoilState(GENDER);
-  const setModelURL = useSetRecoilState(MODEL_URL);
-  const setMetadataURL = useSetRecoilState(METADATA_URL);
-  const setWeightsURL = useSetRecoilState(WEIGHTS_URL);
-  const [isPreviousTypingFinished, setPreviousTypingFinished] = useState(false);
-
-  const handlePreviousTypingFinish = () => {
-    setPreviousTypingFinished(true);  
-  };
-
-  return (
-    <div className="container">
-       <div className="participant-wrapper">
-        <Statistic count="8" most_woman_animal="토끼상" most_man_animal="늑대상" most_woman_path="rabbit-woman" most_man_path="wolf"/>
-      </div>
-      <div className="content-wrapper">
-        <div className="header">
-          <img className="mainLogo" src={`${process.env.PUBLIC_URL}/img/mainLogo.png`} />
+    const navigate = useNavigate();
+    const [selectedGender, setSelectedGender] = useRecoilState(GENDER);
+    const setModelURL = useSetRecoilState(MODEL_URL);
+    const setMetadataURL = useSetRecoilState(METADATA_URL);
+    const setWeightsURL = useSetRecoilState(WEIGHTS_URL);
+    const [isPreviousTypingFinished, setPreviousTypingFinished] = useState(false);
+    const isRow = useMediaQuery({
+      query:"(max-width:2000px)"
+    });
+    const isColumn = useMediaQuery({
+      query:"(max-width:1000px)"
+    });
+  
+    const handlePreviousTypingFinish = () => {
+      setPreviousTypingFinished(true);  
+    };
+  
+    return (
+      <div className="container">
+         <div className="participant-wrapper">
+          {!isRow && (
+            <Statistic count="8" most_woman_animal="토끼상" most_man_animal="늑대상" most_woman_path="rabbit-woman" most_man_path="wolf"/>
+          )}
         </div>
-
-        <div className="main">
-          <div className="character-wrap">
-            <img className="character" src={`${process.env.PUBLIC_URL}/img/character.png`} />
+        <div className="content-wrapper">
+          <div className="header">
+            <img className="mainLogo" src={`${process.env.PUBLIC_URL}/img/mainLogo.png`} />
           </div>
-
-          <div className="speech-wrap">
-            <span className="character-name">너굴</span>
-            <img className="speechBubble" src={`${process.env.PUBLIC_URL}/img/speechBubble.png`} />
-            <div className="speech-text">
-              <p>
-                {isPreviousTypingFinished ? (
-                  "오늘 동물상 미팅을 하러 온 주민이구나"
-                ) : (
-                  <TypingText text="오늘 동물상 미팅을 하러 온 주민이구나" onFinish={handlePreviousTypingFinish} />
-                )}
-              </p>
-              {isPreviousTypingFinished && (
+  
+          <div className="main">
+            <div className="character-wrap">
+              <img className="character" src={`${process.env.PUBLIC_URL}/img/character.png`} />
+            </div>
+  
+            <div className="speech-wrap">
+              <span className="character-name">너굴</span>
+              <img className="speechBubble" src={`${process.env.PUBLIC_URL}/img/speechBubble.png`} />
+              <div className="speech-text">
                 <p>
-                  <TypingText text="성별을 알려 달라구리!" />
+                  {isPreviousTypingFinished ? (
+                    "오늘 동물상 미팅을 하러 온 주민이구나!"
+                  ) : (
+                    <TypingText text="오늘 동물상 미팅을 하러 온 주민이구나!" onFinish={handlePreviousTypingFinish} />
+                  )}
                 </p>
-              )}
+                {isPreviousTypingFinished && (
+                  <p>
+                    <TypingText text="성별을 알려 달라구리!" />
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-
-          <div className="select-wrap">
-            <img
-              className="selectBubble"
-              src={`${process.env.PUBLIC_URL}/img/selectBubble.png`}
-               
-            />
-            <div className="select-text">
-              <p
-                onClick={() => {
-                  setModelURL(process.env.PUBLIC_URL + "/models/girl/model.json");
-                  setMetadataURL(process.env.PUBLIC_URL + "/models/girl/metadata.json");
-                  setWeightsURL(process.env.PUBLIC_URL + "/models/girl/weights.bin");
-                  navigate("/test");
-                }}
-                onMouseEnter={() => setSelectedGender("여자")}
-                onMouseLeave={() => setSelectedGender(null)}
-                className={
-                  selectedGender === "여자"
-                    ? "pointer selected-gender-text selected"
-                    : "pointer selected-gender-text"
-                }
-              >
-                여자
-              </p>
-              <p
-                onClick={() => {
-                  setModelURL(process.env.PUBLIC_URL + "/models/boy/model.json");
-                  setMetadataURL(process.env.PUBLIC_URL + "/models/boy/metadata.json");
-                  setWeightsURL(process.env.PUBLIC_URL + "/models/boy/weights.bin");
-                  navigate("/test");
-                }}
-                onMouseEnter={() => setSelectedGender("남자")}
-                onMouseLeave={() => setSelectedGender(null)}
-                className={
-                  selectedGender === "남자"
-                    ? "pointer selected-gender-text selected"
-                    : "pointer selected-gender-text"
-                }
-              >
-                남자
-              </p>
+  
+            <div className="select-wrap">
+              <img
+                className="selectBubble"
+                src={`${process.env.PUBLIC_URL}/img/selectBubble.png`}
+                 
+              />
+              <div className="select-text">
+                <p
+                  onClick={() => {
+                    setModelURL(process.env.PUBLIC_URL + "/models/girl/model.json");
+                    setMetadataURL(process.env.PUBLIC_URL + "/models/girl/metadata.json");
+                    setWeightsURL(process.env.PUBLIC_URL + "/models/girl/weights.bin");
+                    navigate("/test");
+                  }}
+                  onMouseEnter={() => setSelectedGender("여자")}
+                  onMouseLeave={() => setSelectedGender(null)}
+                  className={
+                    selectedGender === "여자"
+                      ? "pointer selected-gender-text selected"
+                      : "pointer selected-gender-text"
+                  }
+                >
+                  여자
+                </p>
+                <p
+                  onClick={() => {
+                    setModelURL(process.env.PUBLIC_URL + "/models/boy/model.json");
+                    setMetadataURL(process.env.PUBLIC_URL + "/models/boy/metadata.json");
+                    setWeightsURL(process.env.PUBLIC_URL + "/models/boy/weights.bin");
+                    navigate("/test");
+                  }}
+                  onMouseEnter={() => setSelectedGender("남자")}
+                  onMouseLeave={() => setSelectedGender(null)}
+                  className={
+                    selectedGender === "남자"
+                      ? "pointer selected-gender-text selected"
+                      : "pointer selected-gender-text"
+                  }
+                >
+                  남자
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        {!isRow && (
+          <div className="noticeboard-wrapper">
+            <NoticeBoard  />
+          </div>
+        )}
       </div>
-      <div className="noticeboard-wrapper">
-        <NoticeBoard  />
-      </div>
-       
-    </div>
-  );
-};
-
-export default SettingPage;
+    );
+  };
+  
+  export default SettingPage;
