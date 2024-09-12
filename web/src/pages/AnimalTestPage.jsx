a665ce564d3fb579b34bfd8ff0eb7495c326c245
JunYoungKr <kop981020@naver.com>
2024-09-13 01:11:55 +0900

    test

--- web/src/pages/AnimalTestPage.jsx
+++ web/src/pages/AnimalTestPage.jsx
@@ -670,7 +670,7 @@ const ResultBtnContainer = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 15px;
+  gap: 16px;
 `;
 const GoMeetingBtn = styled.button`
   font-size: 18px;
