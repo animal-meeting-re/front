9b9dc2dbb5dacb248e0dd736e1598eac500841dc
JunYoungKr <kop981020@naver.com>
2024-09-23 01:35:58 +0900

    디자인 수정

--- src/components/base/GradientButton.jsx
+++ src/components/base/GradientButton.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./GradientButton.css";
 
 const GradientButton = React.forwardRef((props, ref) => {
-    return (
-        <div 
-            className="gradient_button" 
-            style={props.buttonStyle} 
-            onClick={props.onClick}
-            ref={ref}
-        >
-            <div className="gradient_button_content" style={props.textStyle}>
-                {props.content}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="gradient_button"
+      style={props.buttonStyle}
+      onClick={props.onClick}
+      ref={ref}
+    >
+      <div className="gradient_button_content" style={props.textStyle}>
+        {props.content}
+      </div>
+    </div>
+  );
 });
 
 export default GradientButton;
